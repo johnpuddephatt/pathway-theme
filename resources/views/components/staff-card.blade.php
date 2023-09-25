c0366d622d9b7c35bc91c39978257afb91bdabe0
John Puddephatt <john@jdp.org.uk>
2023-09-25 22:38:37 +0100

    staff grid

--- resources/views/components/staff-card.blade.php
+++ resources/views/components/staff-card.blade.php
@@ -1,6 +1,6 @@
 <a class="flex flex-row items-center gap-4 overflow-hidden rounded bg-beige bg-opacity-30 p-6 !no-underline transition hover:bg-opacity-60 sm:block sm:flex-col"
   href="{{ get_permalink($person->ID) }}">
-  {!! get_the_post_thumbnail($person->ID, 'square', [
+  {!! get_the_post_thumbnail($person->ID, 'square-xs', [
       'class' => ' !my-0 w-32 sm:w-full  h-auto rounded-full',
   ]) !!}
   <div>
