d511487c99d7070b7c49ee0828e58239a79ec27c
John Puddephatt <john@jdp.org.uk>
2023-09-25 22:30:52 +0100

    add title to staff block

--- resources/views/blocks/staff-grid.blade.php
+++ resources/views/blocks/staff-grid.blade.php
@@ -1,6 +1,6 @@
 <div class="wp-block my-16">
 
-  <h2 class="wp-block-heading mb-8">Staff Grid</h2>
+  <h2 class="wp-block-heading mb-8">{{ $layout->title }}</h2>
 
   <div class="grid gap-8 sm:grid-cols-2 md:grid-cols-3">
     @foreach ($staff as $person)
