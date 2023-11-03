1f9c269087f2ac9d088ffbb8485d52addff270d4
John Puddephatt <john@jdp.org.uk>
2023-11-03 21:10:53 +0530

    file

--- resources/views/partials/content-single-resource.blade.php
+++ resources/views/partials/content-single-resource.blade.php
@@ -34,14 +34,19 @@
                 <div>
                   <div class="flex flex-row items-end gap-2">
                     <h3 class="text-xl font-semibold leading-tight">{{ $file_upload['file']->post_title }}</h3>
-                    <div class="mt-2 flex flex-row gap-1.5">
+                    <div
+                      class="mt-2 flex flex-row items-center gap-1.5 rounded bg-beige bg-opacity-20 px-1 text-sm uppercase">
+                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2"
+                        stroke="currentColor" class="h-4 w-4">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                          d="M2.25 12.75V12A2.25 2.25 0 014.5 9.75h15A2.25 2.25 0 0121.75 12v.75m-8.69-6.44l-2.12-2.12a1.5 1.5 0 00-1.061-.44H4.5A2.25 2.25 0 002.25 6v12a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18V9a2.25 2.25 0 00-2.25-2.25h-5.379a1.5 1.5 0 01-1.06-.44z" />
+                      </svg>
+
                       @php($file_type_array = explode('.', $file_upload['file']->guid))
                       @if (count($file_type_array))
-                        <div class="rounded bg-beige bg-opacity-50 px-1 uppercase">.{{ end($file_type_array) }}</div>
+                        <div class="">{{ end($file_type_array) }}</div>
                       @endif
-
-                      {{ max(number_format(filesize(get_attached_file($file_upload['file']->ID)) / 1000000, 1), 0.1) }}MB
-
+                      ({{ max(number_format(filesize(get_attached_file($file_upload['file']->ID)) / 1000000, 1), 0.1) }}MB)
                     </div>
                   </div>
 
