8a4b7e00ff719fc82613545f987f66440827c528
John Puddephatt <john@jdp.org.uk>
2023-11-05 16:32:43 +0530

    flex

--- resources/views/template-resources.blade.php
+++ resources/views/template-resources.blade.php
@@ -26,7 +26,8 @@
   <div x-data="{ show_all: false }" class="bg-beige bg-opacity-20 py-12 text-center">
     <div class="container flex flex-wrap gap-x-8 gap-y-4">
       @foreach ($key_issues as $issue)
-        <a href="{{ get_permalink($issue->ID) }}" class="rounded bg-beige px-6 py-4 text-center md:w-1/2 lg:w-1/3">
+        <a href="{{ get_permalink($issue->ID) }}"
+          class="rounded bg-beige px-6 py-4 text-center md:w-[calc(50%-1rem)] lg:w-[calc(33%-2/3rem)]">
           <h2 class="font-bold">{!! $issue->post_title !!}</h2>
         </a>
       @endforeach
