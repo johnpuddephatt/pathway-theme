3a027d0aaec87d9dc58aba9f5278073f6b2bcf18
John Puddephatt <john@jdp.org.uk>
2023-11-05 09:32:31 +0530

    spacing

--- resources/views/components/resource-card.blade.php
+++ resources/views/components/resource-card.blade.php
@@ -6,9 +6,9 @@
         <h3 class="mb-1 text-xl font-bold !leading-none lg:mb-3 lg:text-2xl">{{ $resource->post_title }}
         </h3>
 
-        <div class="flex flex-col items-start gap-0.5 font-semibold lg:flex-row lg:items-center lg:gap-2">
+        <div class="flex flex-col items-start gap-0.5 font-semibold lg:flex-row lg:items-center lg:gap-1">
 
-          {{ get_the_date('F Y', $resource->ID) }}
+          {{ get_the_date('F Y', $resource->ID) }}&nbsp;
           @foreach (get_the_terms($resource->ID, 'resource_type') as $term)
             <div
               class="hidden rounded-full bg-white bg-opacity-50 px-4 py-0.5 text-sm font-semibold lowercase antialiased lg:inline-block">
