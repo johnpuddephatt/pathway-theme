5b1ac3e95a487ad80b1f62095e0da86131ef71cd
John Puddephatt <john@jdp.org.uk>
2023-11-05 09:02:26 +0530

    date format on resources

--- resources/views/partials/resource-header.blade.php
+++ resources/views/partials/resource-header.blade.php
@@ -4,7 +4,8 @@
     <a class="mb-6 inline-block lg:text-lg"
       href="{{ get_permalink(get_option('page_for_resources')) }}">{!! get_the_title(get_option('page_for_resources')) !!}
       &gt;</a>
-    <h1 class="mb-8 max-w-5xl text-5xl font-bold">{!! $title !!} </h1>
+    <h1 class="mb-8 max-w-4xl text-5xl font-bold">{!! $title !!} </h1>
+    <p>{{ get_the_date('m Y') }}</p>
 
     @if ($types)
       <div class="flex flex-row gap-2">
