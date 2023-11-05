30b6af7b613c0063dcfab3ca7fdaae96146d478f
John Puddephatt <john@jdp.org.uk>
2023-11-05 09:04:44 +0530

    format

--- resources/views/partials/resource-header.blade.php
+++ resources/views/partials/resource-header.blade.php
@@ -5,7 +5,7 @@
       href="{{ get_permalink(get_option('page_for_resources')) }}">{!! get_the_title(get_option('page_for_resources')) !!}
       &gt;</a>
     <h1 class="max-w-4xl text-5xl font-bold">{!! $title !!} </h1>
-    <p class="mb-8 text-xl">{{ get_the_date('F Y') }}</p>
+    <p class="mb-8 mt-2 text-2xl">{{ get_the_date('F Y') }}</p>
 
     @if ($types)
       <div class="flex flex-row gap-2">
