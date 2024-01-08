cace72c9cb999fd764165704f331ad8250787834
John Puddephatt <john@jdp.org.uk>
2024-01-08 16:44:58 +0000

    font size

--- resources/views/components/page-card.blade.php
+++ resources/views/components/page-card.blade.php
@@ -7,7 +7,7 @@
   <div class="py-6 md:px-6 md:pb-8 md:pt-4">
     <h3 class="text-balance text-xl font-bold !leading-tight md:text-2xl">{{ $page->post_title }}</h3>
     <a href="{{ get_permalink($page->ID) }}}"
-      class="inline-block rounded-full bg-blue bg-opacity-70 px-6 py-2 font-semibold !text-white !no-underline transition before:absolute before:inset-0 hover:bg-opacity-100 max-md:h-0 max-md:!p-0 max-md:opacity-0 md:mt-6">Read
+      class="mb:text-base inline-block rounded-full bg-blue bg-opacity-70 px-6 py-2 text-[0px] font-semibold !text-white !no-underline transition before:absolute before:inset-0 hover:bg-opacity-100 max-md:h-0 max-md:!p-0 max-md:opacity-0 md:mt-6">Read
       More</a>
   </div>
 
