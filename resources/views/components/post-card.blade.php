18cb473fb68290b0d37bc96c1b7f6b529f24b68a
John Puddephatt <john@jdp.org.uk>
2024-01-08 15:03:50 +0000

    post card

--- resources/views/components/post-card.blade.php
+++ resources/views/components/post-card.blade.php
@@ -1,5 +1,5 @@
 <div class="group relative flex flex-row items-center rounded bg-beige bg-opacity-50 transition hover:bg-opacity-80">
-  <div class="clip-hex mx-4 my-6 hidden w-20 bg-blue bg-opacity-20 md:block lg:mx-8 lg:w-36">
+  <div class="clip-hex mx-4 my-6 hidden w-20 bg-beige bg-opacity-30 md:block lg:mx-8 lg:w-36">
     {!! get_the_post_thumbnail($post->ID, 'square', [
         'class' => ' h-full w-full object-cover',
     ]) !!}
@@ -8,7 +8,7 @@
     <div class="mb-2 mt-auto font-semibold">
       {{ get_the_date(null, $post->ID) }}
     </div>
-    <h3 class="max-w-3xl text-lg font-semibold !leading-tight md:text-xl">{{ $post->post_title }}</h3>
+    <h3 class="max-w-3xl text-lg font-semibold !leading-tight md:text-xl lg:text-2xl">{{ $post->post_title }}</h3>
     @if ($post->post_excerpt)
       <p class="mt-6 max-w-3xl">{!! $post->post_excerpt !!}</p>
     @endif
