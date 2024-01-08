e46c40a51d4cf76f78ba30a144dcf4486a493b10
John Puddephatt <john@jdp.org.uk>
2024-01-08 15:14:16 +0000

    nudge

--- resources/views/components/post-card.blade.php
+++ resources/views/components/post-card.blade.php
@@ -11,7 +11,7 @@
     </div>
     <h3 class="max-w-3xl text-lg font-semibold !leading-tight md:text-xl lg:text-2xl">{{ $post->post_title }}</h3>
     @if ($post->post_excerpt)
-      <p class="mt-4 max-w-3xl">{!! $post->post_excerpt !!}</p>
+      <p class="mt-3 max-w-3xl">{!! $post->post_excerpt !!}</p>
     @endif
 
   </div>
