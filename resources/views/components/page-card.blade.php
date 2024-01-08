0c0cded12d24d38e0908177cefcea716b64ddad2
John Puddephatt <john@jdp.org.uk>
2024-01-08 16:49:20 +0000

    read more'

--- resources/views/components/page-card.blade.php
+++ resources/views/components/page-card.blade.php
@@ -4,10 +4,10 @@
     class="max-md:clip-hex md:clip-hex-bottom flex-none bg-green bg-opacity-40 max-md:m-2 max-md:w-20 md:w-full lg:aspect-[1.5]">
     {!! get_the_post_thumbnail($page->ID, '3by2', ['class' => ' block h-full object-cover w-full']) !!}
   </div>
-  <div class="py-6 md:px-6 md:pb-8 md:pt-4">
+  <div class="flex py-6 md:block md:px-6 md:pb-8 md:pt-4">
     <h3 class="text-balance text-xl font-bold !leading-tight md:text-2xl">{{ $page->post_title }}</h3>
     <a href="{{ get_permalink($page->ID) }}}"
-      class="mb:text-base rounded-full bg-blue bg-opacity-70 px-6 py-2 text-[0px] font-semibold !text-white !no-underline transition before:absolute before:inset-0 hover:bg-opacity-100 max-md:h-0 max-md:!p-0 max-md:opacity-0 md:mt-6 md:inline-block">Read
+      class="inline-block rounded-full bg-blue bg-opacity-70 px-6 py-2 font-semibold !text-white !no-underline transition before:absolute before:inset-0 hover:bg-opacity-100 max-md:h-0 max-md:w-0 max-md:!p-0 max-md:opacity-0 md:mt-6">Read
       More</a>
   </div>
 
