7e27d1ce6890a1a8abc6d7a9d51c44a004f9db31
John Puddephatt <john@jdp.org.uk>
2024-01-08 14:57:39 +0000

    add excerpt

--- resources/views/components/post-card.blade.php
+++ resources/views/components/post-card.blade.php
@@ -1,12 +1,15 @@
 <div class="group relative flex flex-row items-center rounded bg-beige bg-opacity-50 transition hover:bg-opacity-80">
   <div class="clip-hex mx-4 my-6 hidden w-20 bg-blue bg-opacity-20 md:block lg:mx-8 lg:w-36">
     {!! get_the_post_thumbnail($post->ID, 'square', [
-        'class' => ' h-auto w-full object-cover',
+        'class' => ' h-full w-full object-cover',
     ]) !!}
   </div>
   <div class="flex flex-col items-start p-4 lg:pl-0">
 
     <h3 class="max-w-3xl text-lg font-semibold !leading-tight md:text-xl">{{ $post->post_title }}</h3>
+    @if ($post->post_excerpt)
+      <p>{!! $post->post_excerpt !!}</p>
+    @endif
     <div class="mt-auto md:mb-3 md:mt-1 md:text-lg">
       {{ get_the_date(null, $post->ID) }}
     </div>
