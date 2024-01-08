8a0dbc8f1ef1436f96d2e0e73cc18433aff01647
John Puddephatt <john@jdp.org.uk>
2024-01-08 18:59:54 +0000

    press release card

--- resources/views/components/press_release-card.blade.php
+++ resources/views/components/press_release-card.blade.php
@@ -2,13 +2,12 @@
   class="group relative block overflow-hidden rounded bg-pink bg-opacity-30 !no-underline transition hover:bg-opacity-50">
   <div class="flex flex-row items-center">
     <div class="p-4">
-
-      <h3 class="!my-0 mb-1 text-lg font-semibold !leading-tight md:text-xl">{{ $release->post_title }}
-      </h3>
-
-      <div class="text-blue">
+      <div class="mb-2 mt-auto font-semibold">
         {{ get_the_date(null, $release->ID) }}
       </div>
+
+      <h3 class="!my-0 mb-1 text-lg font-semibold !leading-tight md:text-xl lg:text-2xl">{{ $release->post_title }}
+      </h3>
 
     </div>
 
