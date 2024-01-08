4a323d4f59e8c75bfe9ee886cea094bfa520fec3
John Puddephatt <john@jdp.org.uk>
2024-01-08 15:06:39 +0000

    post card

--- resources/views/components/post-card.blade.php
+++ resources/views/components/post-card.blade.php
@@ -1,4 +1,5 @@
-<div class="group relative flex flex-row items-center rounded bg-beige bg-opacity-50 transition hover:bg-opacity-80">
+<a href="{{ get_permalink($post->ID) }}"
+  class="group relative flex flex-row items-center rounded bg-beige bg-opacity-50 transition hover:bg-opacity-80">
   <div class="clip-hex mx-4 my-6 hidden w-20 bg-beige md:block lg:mx-8 lg:w-36">
     {!! get_the_post_thumbnail($post->ID, 'square', [
         'class' => ' h-full w-full object-cover',
@@ -13,11 +14,8 @@
       <p class="mt-4 max-w-3xl">{!! $post->post_excerpt !!}</p>
     @endif
 
-    {{-- <a href="{{ get_permalink($post->ID) }}"
-      class="mt-auto rounded-full bg-blue bg-opacity-80 px-6 py-2 text-sm font-semibold text-white transition duration-300 before:absolute before:inset-0 hover:bg-opacity-100 max-md:h-0 max-md:!p-0 max-md:opacity-0 md:inline-block">
-      Read More</a> --}}
   </div>
 
   @include('partials.card-arrow', ['class' => 'group-hover:bg-beige'])
 
-</div>
+</a>
