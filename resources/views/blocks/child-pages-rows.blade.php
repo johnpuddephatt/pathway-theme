5d46809bc5d5629bdab7550de9a2b427f13ca745
John Puddephatt <john@jdp.org.uk>
2024-01-09 18:01:06 +0000

    child pages

--- resources/views/blocks/child-pages-rows.blade.php
+++ resources/views/blocks/child-pages-rows.blade.php
@@ -1,4 +1,4 @@
-<div class="relative overflow-hidden py-8 md:py-16">
+<div class="wp-block relative overflow-hidden py-8 md:py-16">
 
   <div class="flex flex-col gap-4">
     @foreach ($child_pages as $page)
