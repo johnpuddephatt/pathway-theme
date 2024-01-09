b7219196f1bb79677d2a1a1a26b96b628477b88a
John Puddephatt <john@jdp.org.uk>
2024-01-09 12:43:13 +0000

    accordion styling

--- bud.config.js
+++ bud.config.js
@@ -34,7 +34,7 @@ export default async (app) => {
    */
   app
     .setUrl('http://localhost:3000')
-    .setProxyUrl('http://example.test')
+    .setProxyUrl('http://pathway.test')
     .watch(['resources/views', 'app']);
 
   /**
