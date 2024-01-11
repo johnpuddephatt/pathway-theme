eda6fe9a03768017cad1bdae1b892b6425f6e8a9
John Puddephatt <john@jdp.org.uk>
2024-01-11 13:25:32 +0000

    staff grid

--- resources/views/components/staff-card.blade.php
+++ resources/views/components/staff-card.blade.php
@@ -1,10 +1,11 @@
-<a class="flex flex-row items-center gap-4 overflow-hidden rounded bg-beige bg-opacity-30 p-6 !no-underline transition hover:bg-opacity-60 sm:block sm:flex-col"
+<a class="not-prose flex flex-row items-center gap-4 overflow-hidden rounded bg-beige bg-opacity-30 p-6 !no-underline transition hover:bg-opacity-60 sm:block sm:flex-col"
   href="{{ get_permalink($person->ID) }}">
-  {!! get_the_post_thumbnail($person->ID, 'square-xs', [
-      'class' => ' !my-0 w-32 sm:w-full  h-auto rounded-full',
-  ]) !!}
-  <div>
-    <h3 class="!mb-1 !mt-2 !text-xl sm:!text-lg">{{ $person->post_title }}</h3>
-    <div class="!text-lg leading-none sm:!text-base"> {{ carbon_get_post_meta($person->ID, 'role_title') }}</div>
-  </div>
+  <div class="h-32 w-32 overflow-hidden rounded-full bg-beige bg-opacity-50">
+    {!! get_the_post_thumbnail($person->ID, 'square-xs', [
+        'class' => ' !my-0 w-32 h-32',
+    ]) !!}
+    <div>
+      <h3 class="mb-1 mt-2 text-lg font-semibold leading-none">{{ $person->post_title }}</h3>
+      <div class="leading-none"> {{ carbon_get_post_meta($person->ID, 'role_title') }}</div>
+    </div>
 </a>
