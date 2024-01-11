d62573c7b93dbd4d2129c2baa93d33920af35325
John Puddephatt <john@jdp.org.uk>
2024-01-11 12:46:46 +0000

    pathway

--- resources/views/blocks/staff-grid.blade.php
+++ resources/views/blocks/staff-grid.blade.php
@@ -2,6 +2,10 @@
 
   <h2 class="wp-block-heading mb-8">{{ $fields->title ?? 'Staff' }}</h2>
 
+  @if ($fields->description)
+    <p>{{ $fields->description }}</p>
+  @endif
+
   <div class="grid gap-8 sm:grid-cols-2 md:grid-cols-3">
     @foreach ($staff as $person)
       @include('components.staff-card', ['person' => $person])
