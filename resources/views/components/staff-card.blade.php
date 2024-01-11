02fd786ff2e180c521c1aa8cfdb54f6ca3e38cf6
John Puddephatt <john@jdp.org.uk>
2024-01-11 13:27:33 +0000

    staff

--- resources/views/components/staff-card.blade.php
+++ resources/views/components/staff-card.blade.php
@@ -1,12 +1,12 @@
 <a class="not-prose flex flex-row items-center gap-4 overflow-hidden rounded bg-beige bg-opacity-30 p-6 !no-underline transition hover:bg-opacity-60 sm:block sm:flex-col"
   href="{{ get_permalink($person->ID) }}">
-  <div class="h-32 w-32 overflow-hidden rounded-full bg-beige bg-opacity-50">
+  <div class="mx-auto mb-4 h-32 w-32 overflow-hidden rounded-full bg-beige bg-opacity-50">
     {!! get_the_post_thumbnail($person->ID, 'square-xs', [
         'class' => ' !my-0 w-32 h-32',
     ]) !!}
   </div>
   <div>
     <h3 class="mb-1 mt-2 text-lg font-semibold leading-none">{{ $person->post_title }}</h3>
-    <div class="leading-none"> {{ carbon_get_post_meta($person->ID, 'role_title') }}</div>
+    <div class="text-base font-normal leading-none"> {{ carbon_get_post_meta($person->ID, 'role_title') }}</div>
   </div>
 </a>
