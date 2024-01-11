681d18c8759d3bc1dd6017ba5c1a9f1036739a19
John Puddephatt <john@jdp.org.uk>
2024-01-11 12:29:04 +0000

    resource signpost

--- resources/views/blocks/resource-signpost.blade.php
+++ resources/views/blocks/resource-signpost.blade.php
@@ -7,10 +7,10 @@
         class="clip-hex ml-6 hidden w-20 flex-none bg-yellow bg-opacity-70 transition group-hover:bg-opacity-100 md:block">
       </div> --}}
       <div class="flex-1 px-6 py-4">
-        <div class="text-sm font-semibold leading-tight antialiased">Research, Policy &amp; Practice &gt;
+        <div class="mb-1 leading-tight">Research, Policy &amp; Practice &gt;
         </div>
 
-        <h3 class="mb-1 text-xl font-semibold !leading-tight">{{ $fields->title ?: $resource->post_title }}
+        <h3 class="mb-1 text-lg font-semibold !leading-tight lg:text-xl">{{ $fields->title ?: $resource->post_title }}
         </h3>
 
         {{-- <span class="inline-block rounded-full bg-white px-4 py-1 text-sm">Access this resource</span> --}}
