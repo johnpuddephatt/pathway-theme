2065bd86e32aba0bded9c618e6965c3162464850
John Puddephatt <john@jdp.org.uk>
2024-01-11 13:58:19 +0000

    grid

--- resources/views/components/staff-card.blade.php
+++ resources/views/components/staff-card.blade.php
@@ -1,8 +1,8 @@
 <a class="not-prose flex flex-row items-center gap-4 overflow-hidden rounded bg-beige bg-opacity-30 p-4 !no-underline transition hover:bg-opacity-60 xl:text-base 2xl:px-6"
   href="{{ get_permalink($person->ID) }}">
-  <div class="h-32 w-32 overflow-hidden rounded-full bg-beige bg-opacity-50">
+  <div class="h-24 w-24 overflow-hidden rounded-full bg-beige bg-opacity-50">
     {!! get_the_post_thumbnail($person->ID, 'square-xs', [
-        'class' => ' !my-0 w-32 h-32',
+        'class' => ' !my-0 w-24 h-24',
     ]) !!}
   </div>
   <div>
