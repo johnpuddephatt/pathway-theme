ceca3b4d9078934bb816547ecfe835c8a23d1879
John Puddephatt <john@jdp.org.uk>
2024-01-31 11:47:18 +0000

    event tab logic

--- resources/views/template-events.blade.php
+++ resources/views/template-events.blade.php
@@ -10,20 +10,22 @@
       {!! the_content() !!}
     </div>
 
-    <div class="flex flex-row items-center justify-end gap-2">
+    @if ($events->have_posts() && $past_events->have_posts())
+      <div class="flex flex-row items-center justify-end gap-2">
 
-      <a href="{{ get_permalink() }}?past=1"
-        class="{{ isset($_GET['past']) ? 'bg-blue text-white' : '' }} inline-block rounded-full bg-opacity-80 px-10 py-2.5 font-semibold transition duration-300 hover:bg-opacity-100">
-        Past Events
-      </a>
-      <a href="{{ get_permalink() }}"
-        class="{{ isset($_GET['past']) ? '' : 'bg-blue text-white' }} inline-block rounded-full bg-opacity-80 px-10 py-2.5 font-semibold transition duration-300 hover:bg-opacity-100">
-        Upcoming Events
-      </a>
+        <a href="{{ get_permalink() }}?past=1"
+          class="{{ isset($_GET['past']) ? 'bg-blue text-white' : '' }} inline-block rounded-full bg-opacity-80 px-10 py-2.5 font-semibold transition duration-300 hover:bg-opacity-100">
+          Past Events
+        </a>
+        <a href="{{ get_permalink() }}"
+          class="{{ isset($_GET['past']) ? '' : 'bg-blue text-white' }} inline-block rounded-full bg-opacity-80 px-10 py-2.5 font-semibold transition duration-300 hover:bg-opacity-100">
+          Upcoming Events
+        </a>
 
-    </div>
+      </div>
+    @endif
 
-    @if (isset($_GET['past']))
+    @if (isset($_GET['past']) || ($past_events->have_posts() && !$events->have_posts()))
 
       @if (!$past_events->have_posts())
         <div class="my-16">
@@ -43,7 +45,7 @@
       @if (!$events->have_posts())
         <div class="my-16">
           <x-alert type="warning">
-            Sorry, no events were found.
+            Sorry, no upcoming events were found.
           </x-alert>
         </div>
       @else
