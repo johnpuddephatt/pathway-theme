6e62a0f9ff529e0ad79b2d12e8454966298dc0e0
John Puddephatt <john@jdp.org.uk>
2024-02-13 19:50:14 +0000

    page header

--- resources/views/partials/page-header.blade.php
+++ resources/views/partials/page-header.blade.php
@@ -1,32 +1,34 @@
-<div>
-  <div class="container mt-8 md:mt-12 xl:max-w-5xl 2xl:max-w-6xl">
-    <div class="relative">
+@if (isset($post))
+  <div>
+    <div class="container mt-8 md:mt-12 xl:max-w-5xl 2xl:max-w-6xl">
+      <div class="relative">
 
-      @if (isset($post) && isset($post->post_type) && $post->post_type == 'post')
-        <div class="mb-4 text-xl font-bold md:mb-8 md:text-2xl">
-          {{ get_the_date() }}
-        </div>
-      @elseif(isset($post))
-        @php($ancestors = array_reverse(get_post_ancestors($post->ID)))
-        <div class="mb-4">
-          @foreach ($ancestors as $ancestor)
-            <a href="{{ get_permalink($ancestor) }}">{{ get_the_title($ancestor) }}</a> &gt;
-          @endforeach
-        </div>
-      @endif
+        @if (isset($post) && isset($post->post_type) && $post->post_type == 'post')
+          <div class="mb-4 text-xl font-bold md:mb-8 md:text-2xl">
+            {{ get_the_date() }}
+          </div>
+        @else
+          @php($ancestors = array_reverse(get_post_ancestors($post->ID)))
+          <div class="mb-4">
+            @foreach ($ancestors as $ancestor)
+              <a href="{{ get_permalink($ancestor) }}">{{ get_the_title($ancestor) }}</a> &gt;
+            @endforeach
+          </div>
+        @endif
 
-      <h2 class="mb-8 max-w-3xl font-serif text-5xl text-blue xl:text-6xl">
-        {!! $title !!}
-      </h2>
+        <h2 class="mb-8 max-w-3xl font-serif text-5xl text-blue xl:text-6xl">
+          {!! $title !!}
+        </h2>
 
-      @if (!empty($post->post_excerpt))
-        <p class="mb-8 max-w-2xl text-xl leading-tight text-blue lg:text-xl">
-          {!! $post->post_excerpt !!}
-        </p>
-      @endif
+        @if (!empty($post->post_excerpt))
+          <p class="mb-8 max-w-2xl text-xl leading-tight text-blue lg:text-xl">
+            {!! $post->post_excerpt !!}
+          </p>
+        @endif
 
-      {!! get_the_post_thumbnail($post->ID, '2by1', ['class' => ' rounded mb-8 block h-auto w-full']) !!}
+        {!! get_the_post_thumbnail($post->ID, '2by1', ['class' => ' rounded mb-8 block h-auto w-full']) !!}
 
+      </div>
     </div>
   </div>
-</div>
+@endif
