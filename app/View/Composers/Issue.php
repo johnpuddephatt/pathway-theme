64fac99f1ad9efc3cbd5d12e7dd1492f9492dc78
John Puddephatt <john@jdp.org.uk>
2024-04-19 16:03:55 +0100

    reverse order on key issues page

--- app/View/Composers/Issue.php
+++ app/View/Composers/Issue.php
@@ -13,26 +13,26 @@ class Issue extends Composer
      * @var array
      */
     protected static $views = [
-    
+
         'partials.content-single-issue',
 
     ];
 
-    public function with() {
+    public function with()
+    {
 
         return [
             'resources' => get_posts([
                 'post_type' => 'resource',
                 'posts_per_page' => -1,
-                'order' => 'ASC',
+                'order' => 'DESC',
                 'meta_query' => [
                     [
-                    'key' => 'issues',
-                    'value' => 'post:issue:' . get_the_ID(),
+                        'key' => 'issues',
+                        'value' => 'post:issue:' . get_the_ID(),
                     ],
                 ]
             ]),
         ];
-      
     }
 }
